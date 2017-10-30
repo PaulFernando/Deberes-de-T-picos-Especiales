136bf50eaf3269811c5da878a9f30777c492bcf4
Paúl Fernando Guamaní Morales <paul_fercho_3013@hotmail.com>
2017-10-30 16:43:05 -0600

    validacion con JS
    validacion con JS

--- Curriculum-JavaScript/js/validaciones.js
+++ Curriculum-JavaScript/js/validaciones.js
@@ -2,23 +2,65 @@
  * Created by paul on 27/10/2017.
  */
 
+function validar(formulario) {
+
+    var expEmail;
+    var expfecha;
+    var expCedula;
+
+    expEmail = /\w+@\w+\.[a-z]/;
+    expfecha = /^(0[1-9]|[1-2]\d|3[01])(\/)(0[1-9]|1[012])\2(\d{4})$/;
+    expCedula = /(\d{10})$/;
 
 
-function validar() {
+    if ((formulario.nombre.value.length == 0) || (formulario.apellido.value.length == 0) || (formulario.date.value.length == 0) || (formulario.nacionalidad.value.length == 0) || (formulario.cedula.value.length == 0) || (formulario.estado.value.length == 0) || (formulario.direccion.value.length == 0) || (formulario.telefono.value.length == 0) || (formulario.email.value.length == 0 || (formulario.area.value.length == 0) || (formulario.exampleInputFile.value.length == 0))){
 
-    //vamos a validar mediante el id
-    var nombre, apellido, cedula,telefono, email, expresion;
-
-    //almacenamos los valores de los imputs de las variables
-    nombre = document.getElementById("nombre").value;
-    apellido = document.getElementById("apellido").value;
-    cedula = document.getElementById("cedula").value;
-    telefono = document.getElementById("telefono").value;
-    email = document.getElementById("email").value;
-
-    if (nombre === "") {
-        alert("Llene el campo Nombre");
+        alert("FALTA INFORMACION EN LOS CAMPOS !!!");
         return false;
+
+    }
+
+    else if ((formulario.nombre.value.length > 20)){
+
+        alert('Nombre muy Largo!!!!');
+        return false;
+    }
+
+    else if ((formulario.apellido.value.length > 20)){
+        alert('Apellido muy Largo!!!!');
+        return false;
+    }
+
+
+    else if ((!expfecha.test(formulario.date.value))){
+
+        alert("fecha no es correcta");
+        return false;
+
+    }
+
+    else if ((formulario.nacionalidad.value.length > 20)){
+        alert('Nacionalidad muy Largo!!!!');
+        return false;
+    }
+
+    else if ((!expCedula.test(formulario.cedula.value)) || (formulario.cedula.value.length > 10)){
+        alert('Numero de cedula falso ingrese correctamente');
+        return false;
+
+    }
+
+    else if (isNaN(formulario.telefono.value) || (formulario.telefono.value.length > 20)){
+        alert("En el telefono no se aceptan letras o no es correcto");
+        return false;
+
+    }
+
+    else if ((!expEmail.test(formulario.email.value))){
+
+        alert("el correo no es valido");
+        return false;
+
     }
 
 }
